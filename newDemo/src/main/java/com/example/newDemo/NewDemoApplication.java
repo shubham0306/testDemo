5e6380f3f8a2639c8413d88bad928d22c2d251a2
shubham0306 <56184833+shubham0306@users.noreply.github.com>
2021-07-08 20:35:33 +0530

    Test1

---
+++ newDemo/src/main/java/com/example/newDemo/NewDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.newDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDemoApplication.class, args);
+		System.out.println("....Hi....");
+
+
+	}
+
+}
